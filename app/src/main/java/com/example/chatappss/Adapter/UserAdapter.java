d7ea981828a33c4d3f251371758456aaf690ab36
Saakshi <saakshi.c18@bmsce.ac.in>
2020-11-19 23:30:55 +0530

    messege sending + few errors to be fixed

--- app/src/main/java/com/example/chatappss/Adapter/UserAdapter.java
+++ app/src/main/java/com/example/chatappss/Adapter/UserAdapter.java
@@ -7,13 +7,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import com.example.chatappss.Model.User;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 
 import com.example.chatappss.MessageActivity;
-import com.example.chatappss.Model.User;
 import com.example.chatappss.R;
 
 import java.text.BreakIterator;
--- app/src/main/java/com/example/chatappss/MessageActivity.java
+++ app/src/main/java/com/example/chatappss/MessageActivity.java
@@ -2,18 +2,24 @@ package com.example.chatappss;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 //import android.support.v7.widget.Toolbar;
 
+import com.example.chatappss.Adapter.MessageAdapter;
+import com.example.chatappss.Model.Chat;
 import com.example.chatappss.Model.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -23,7 +29,9 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -37,6 +45,10 @@ public class MessageActivity extends AppCompatActivity {
     EditText entered_message;
     ImageButton send_button;
     Intent intent;
+
+    MessageAdapter messageAdapter;
+    List<Chat>  mChat;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +68,12 @@ public class MessageActivity extends AppCompatActivity {
 //                }
 //            });
 
+        recyclerView=findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+
         profile_image=findViewById(R.id.profile_image);
         username=findViewById(R.id.username);
 
@@ -67,26 +85,6 @@ public class MessageActivity extends AppCompatActivity {
         final String userid=intent.getStringExtra("userid");
         fuser= FirebaseAuth.getInstance().getCurrentUser();
         reference= FirebaseDatabase.getInstance().getReference("Users").child(userid);
-
-        reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                User user=dataSnapshot.getValue(User.class);
-                username.setText(user.getUsername());
-                if(user.getImageUrl().equals("default"))
-                {
-                    profile_image.setImageResource(R.drawable.user);
-                }
-                else{
-                    //Picasso.
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
 
         send_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -101,6 +99,30 @@ public class MessageActivity extends AppCompatActivity {
                 entered_message.setText("");
             }
         });
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user=dataSnapshot.getValue(User.class);
+                Log.i("MessageActivity",user.getUsername());
+                username.setText(user.getUsername());
+                if(user.getImageUrl().equals("default"))
+                {
+                    profile_image.setImageResource(R.drawable.user);
+                }
+                else{
+                    //Picasso.
+                }
+                readMessage(fuser.getUid(),userid,user.getImageUrl());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
         }
         public void sendMessage(String sender,String receiver,String message)
         {
@@ -110,6 +132,48 @@ public class MessageActivity extends AppCompatActivity {
             hashMap.put("receiver",receiver);
             hashMap.put("message",message);
             reference.child("Chats").push().setValue(hashMap);
+        }
+
+        private void readMessage(final String myid, final String userid, final String imageurl)
+        {
+            Log.i("Message Activity","read Message()");
+            mChat=new ArrayList<>();
+            reference=FirebaseDatabase.getInstance().getReference("Chats");
+            reference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    mChat.clear();
+                    for(DataSnapshot snapshot : dataSnapshot.getChildren())
+                    {
+                        Chat chat=snapshot.getValue(Chat.class);
+                        Log.i("Message Activity mess:",chat.getMessage());
+                        Log.i("Message Activity send:",chat.getSender());
+                        Log.i("Message Activity rec",chat.getReceiver());
+                        Log.i("myid ",myid);
+                        Log.i("userid",userid);
+                        if((chat.getReceiver().trim().equals(myid) && chat.getSender().trim().equals(userid))||
+                                (chat.getReceiver().trim().equals(myid) && chat.getSender().trim().equals(userid)))
+                        {
+                            mChat.add(chat);
+                        }
+
+                        if((chat.getSender().trim()==myid.trim())||(chat.getSender().trim()==userid.trim()))
+                        {
+                            mChat.add(chat);
+                        }
+
+
+                        messageAdapter=new MessageAdapter(MessageActivity.this,mChat,imageurl);
+                        recyclerView.setAdapter(messageAdapter);
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
         }
 
     }
